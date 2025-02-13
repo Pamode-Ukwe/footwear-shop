b429b8a3314d22108e8b9a9179d23b016b76e8da
Pamode <ebenezerukwe@gmail.com>
2025-02-13 01:47:12 +0100

    Fixed image loading errors

--- src/productList.ts
+++ src/productList.ts
@@ -1,5 +1,27 @@
-import x from '../src/assets/Black crocs.png'
-
+import a from '../src/assets/Black crocs.png'
+import b from '../src/assets/red.png'
+import c from '../src/assets/Frame green and red.png'
+import d from '../src/assets/fresh white.png'
+import e from '../src/assets/Frame red and white.png'
+import f from '../src/assets/image 163.png'
+import g from '../src/assets/Frame black.png'
+import h from '../src/assets/Frame black and white.png'
+import i from '../src/assets/Frame 1000004200.png'
+import j from '../src/assets/5.png'
+import k from '../src/assets/6.png'
+import l from '../src/assets/red.png'
+import m from '../src/assets/Frame black.png'
+import n from '../src/assets/image 163.png'
+import o from '../src/assets/Frame red and white.png'
+import p from '../src/assets/Frame black and white.png'
+import q from '../src/assets/Black crocs.png'
+import r from '../src/assets/red.png'
+import s from '../src/assets/Frame green and red.png'
+import t from '../src/assets/fresh white.png'
+import u from '../src/assets/Frame red and white.png'
+import v from '../src/assets/image 163.png'
+import w from '../src/assets/Frame black.png'
+import x from '../src/assets/Frame black and white.png'
 export interface Product {
     id: string
     image: string
@@ -11,28 +33,28 @@ export interface Product {
 }
 
 export const productList: Product[] = [
-    {id: 'aa',image: x, name: 'Jordan Roam', price: 35, brand: 'Nike', quantity: 1, subTotal: 35},
-    {id: 'ab',image: '../src/assets/red.png', name: 'Air Jordan 5 Retro SE', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
-    {id: 'ac',image: '../src/assets/Frame green and red.png', name: ' Air Jordan 1 Low G', price: 60, brand: 'Nike', quantity: 1, subTotal: 60},
-    {id: 'ad',image: '../src/assets/Fresh white.png', name: 'Air Jordan 6 Retro', price: 165, brand: 'Nike', quantity: 1, subTotal: 165},
-    {id: 'ae',image: '../src/assets/Frame red and white.png', name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
-    {id: 'af',image: '../src/assets/image 163.png', name: 'Air Jordan 5 Retro SE', price: 144, brand: 'Nike', quantity: 1, subTotal: 144},
-    {id: 'ag',image: '../src/assets/Frame black.png', name: 'Jordan Max Aura 6', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
-    {id: 'ah',image: '../src/assets/Frame black and white.png', name: 'Air Jordan 1 Retro OG', price: 120, brand: 'Nike', quantity: 1, subTotal: 120},
-    {id: 'ai',image: '../src/assets/Frame 1000004200.png', name: 'Air Jordan 1 Element', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
-    {id: 'aj',image: '../src/assets/5.png', name: ' Air Jordan 1 Mid SE', price: 68, brand: 'Nike', quantity: 1, subTotal: 68},
-    {id: 'ak',image: '../src/assets/6.png', name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
-    {id: 'al',image: '../src/assets/red.png', name: 'Air Jordan 5 Retro SE', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
-    {id: 'am',image: '../src/assets/Frame black.png', name: 'Jordan Max Aura 6', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
-    {id: 'an',image: '../src/assets/image 163.png', name: 'Air Jordan 5 Retro SE', price: 144, brand: 'Nike', quantity: 1, subTotal: 144},
-    {id: 'ao',image: '../src/assets/Frame red and white.png', name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
-    {id: 'ap',image: '../src/assets/Frame black and white.png', name: 'Air Jordan 1 Retro OG', price: 120, brand: 'Nike', quantity: 1, subTotal: 120},
-    {id: 'aq',image: '../src/assets/Black crocs.png', name: 'Jordan Roam', price: 35, brand: 'Nike', quantity: 1, subTotal: 35},
-    {id: 'ar',image: '../src/assets/red.png', name: 'Air Jordan 5 Retro SE', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
-    {id: 'as',image: '../src/assets/Frame green and red.png', name: ' Air Jordan 1 Low G', price: 60, brand: 'Nike', quantity: 1, subTotal: 60},
-    {id: 'at',image: '../src/assets/Fresh white.png', name: 'Air Jordan 6 Retro', price: 165, brand: 'Nike', quantity: 1, subTotal: 165},
-    {id: 'au',image: '../src/assets/Frame red and white.png', name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
-    {id: 'av',image: '../src/assets/image 163.png', name: 'Air Jordan 5 Retro SE', price: 144, brand: 'Nike', quantity: 1, subTotal: 144},
-    {id: 'aw',image: '../src/assets/Frame black.png', name: 'Jordan Max Aura 6', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
-    {id: 'ax',image: '../src/assets/Frame black and white.png', name: 'Air Jordan 1 Retro OG', price: 120, brand: 'Nike', quantity: 1, subTotal: 120},
+    {id: 'aa',image: a, name: 'Jordan Roam', price: 35, brand: 'Nike', quantity: 1, subTotal: 35},
+    {id: 'ab',image: b, name: 'Air Jordan 5 Retro SE', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
+    {id: 'ac',image: c, name: ' Air Jordan 1 Low G', price: 60, brand: 'Nike', quantity: 1, subTotal: 60},
+    {id: 'ad',image: d, name: 'Air Jordan 6 Retro', price: 165, brand: 'Nike', quantity: 1, subTotal: 165},
+    {id: 'ae',image: e, name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
+    {id: 'af',image: f, name: 'Air Jordan 5 Retro SE', price: 144, brand: 'Nike', quantity: 1, subTotal: 144},
+    {id: 'ag',image: g, name: 'Jordan Max Aura 6', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
+    {id: 'ah',image: h, name: 'Air Jordan 1 Retro OG', price: 120, brand: 'Nike', quantity: 1, subTotal: 120},
+    {id: 'ai',image: i, name: 'Air Jordan 1 Element', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
+    {id: 'aj',image: j, name: ' Air Jordan 1 Mid SE', price: 68, brand: 'Nike', quantity: 1, subTotal: 68},
+    {id: 'ak',image: k, name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
+    {id: 'al',image: l, name: 'Air Jordan 5 Retro SE', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
+    {id: 'am',image: m, name: 'Jordan Max Aura 6', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
+    {id: 'an',image: n, name: 'Air Jordan 5 Retro SE', price: 144, brand: 'Nike', quantity: 1, subTotal: 144},
+    {id: 'ao',image: o, name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
+    {id: 'ap',image: p, name: 'Air Jordan 1 Retro OG', price: 120, brand: 'Nike', quantity: 1, subTotal: 120},
+    {id: 'aq',image: q, name: 'Jordan Roam', price: 35, brand: 'Nike', quantity: 1, subTotal: 35},
+    {id: 'ar',image: r, name: 'Air Jordan 5 Retro SE', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
+    {id: 'as',image: s, name: ' Air Jordan 1 Low G', price: 60, brand: 'Nike', quantity: 1, subTotal: 60},
+    {id: 'at',image: t, name: 'Air Jordan 6 Retro', price: 165, brand: 'Nike', quantity: 1, subTotal: 165},
+    {id: 'au',image: u, name: 'Jordan Spizike Low', price: 134, brand: 'Nike', quantity: 1, subTotal: 134},
+    {id: 'av',image: v, name: 'Air Jordan 5 Retro SE', price: 144, brand: 'Nike', quantity: 1, subTotal: 144},
+    {id: 'aw',image: w, name: 'Jordan Max Aura 6', price: 125, brand: 'Nike', quantity: 1, subTotal: 125},
+    {id: 'ax',image: x, name: 'Air Jordan 1 Retro OG', price: 120, brand: 'Nike', quantity: 1, subTotal: 120},
 ]
